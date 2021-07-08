b96217fb25e5ffed6ef62f1abd78ffb390812cd0
Nathaniel Shirts <nshirts7@gmail.com>
2021-07-07 21:27:25 -0600

    sets up the database connection

---
+++ server/db/seed.sql
@@ -0,0 +1,51 @@
+CREATE TABLE users (
+    users_id serial PRIMARY KEY,
+    users_first_name VARCHAR(60),
+    users_last_name VARCHAR(60),
+    users_password VARCHAR(300) NOT NULL,
+    phone_number VARCHAR(10)
+);
+
+CREATE TABLE users_picture (
+    users_picture_id serial PRIMARY KEY,
+    users_id int REFERENCES users(users_id)
+);
+
+CREATE TABLE trip (
+    trip_id serial PRIMARY KEY,
+    users_id int REFERENCES users(users_id),
+    trip_name VARCHAR(60)
+);
+
+CREATE TABLE to_do_list (
+    to_do_list_id serial PRIMARY KEY,
+    trip_id int REFERENCES trip(trip_id)
+);
+
+CREATE TABLE to_do_list_item (
+    to_do_list_item_id serial PRIMARY KEY,
+    to_do_list_id int REFERENCES to_do_list(to_do_list_id),
+    item_name VARCHAR(200) NOT NULL,
+    is_done BOOLEAN
+);
+
+CREATE TABLE people_list (
+    people_list_id serial PRIMARY KEY,
+    trip_id int REFERENCES trip(trip_id)
+);
+
+CREATE TABLE people (
+    people_id serial PRIMARY KEY,
+    people_list_id int REFERENCES people_list(people_list_id),
+    first_name VARCHAR(60),
+    last_name VARCHAR(60),
+    email VARCHAR(100),
+    phone_number VARCHAR(10)
+
+);
+
+CREATE TABLE pictures (
+    picture_id serial PRIMARY KEY,
+    trip_id int REFERENCES trip(trip_id),
+    users_id int REFERENCES users(users_id)
+)
--- server/index.js
+++ server/index.js
@@ -1,11 +1,24 @@
 require('dotenv').config();
 const express = require('express');
-const { SERVER_PORT } = process.env;
+const massive = require('massive');
+const { SERVER_PORT, CONNECTION_STRING, SECRET } = process.env;
 
 const app = express();
+
+massive({
+  connectionString: CONNECTION_STRING,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+}).then((dbInstance) => {
+  app.set('db', dbInstance);
+  console.log('The database is running');
+});
+
+app.use(express.json());
 
 app.get('/api/test', (req, res) => {
   res.status(200).send('this works');
 });
 
-app.listen(SERVER_PORT, () => console.log(`Running on Port ${SERVER_PORT}`));
+app.listen(SERVER_PORT, () => console.log(`Running on port ${SERVER_PORT}`));
